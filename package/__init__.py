f20c8287c83724b59b7284fe459e28ec4c12256d
Shubhodippal <shubhodippal01@gmail.com>
2025-01-11 01:23:33 +0530

    Remove package template files and add audio feature extraction functionality

---
+++ package/__init__.py
@@ -0,0 +1,3 @@
+from .data_processing import process_audio_dataset
+
+__all__ = ['process_audio_dataset']
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="audio_feature_extractor",
+    version="0.1.0",
+    description="A package for extracting audio features with data augmentation.",
+    author="Shubhodip Pal",
+    author_email="shubhodippal01@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "numpy==2.0.2",
+        "librosa==0.10.2.post1",
+        "pandas==2.2.3",
+        "tqdm==4.67.1"
+    ],
+    python_requires=">=3.9, <3.13",
+)
